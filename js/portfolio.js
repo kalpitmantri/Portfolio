4a7130df5aec715f1725335e83ecbe06f4bbf79e
Kalpit Mantri <kalpit.mantri1798@gmail.com>
2017-07-25 15:53:26 +0530

    play-pause added

--- js/portfolio.js
+++ js/portfolio.js
@@ -22,5 +22,24 @@ $(window).on('load',function(){
 	$("#sendButton").click(function(){
 		alert("Your message has been sent");
 	});
+
+	$("#mycarousel").carousel({interval:1000});
+
+ 	$("#carousel-button").click(function(){
+                
+        if ($("#carousel-button").children("span").hasClass("fa-pause")) 
+        {
+            $("#mycarousel").carousel("pause");
+            $("#carousel-button").children("span").removeClass("fa-pause");
+            $("#carousel-button").children("span").addClass("fa-play");
+        }
+
+        else if ($("#carousel-button").children("span").hasClass("fa-play")) 
+        {
+            $("#mycarousel").carousel("cycle");
+            $("#carousel-button").children("span").removeClass("fa-play");
+            $("#carousel-button").children("span").addClass("fa-pause");
+        }      
+    });
 });
 
